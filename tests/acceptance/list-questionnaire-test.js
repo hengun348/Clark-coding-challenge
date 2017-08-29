782a324d92852948b1b3a70228ccdf19726bc448
Henrik Gunnarsson <hengun348@gmail.com>
2017-08-29 22:17:49 +0200

    tests(questionnaries): acceptance test

---
+++ tests/acceptance/list-questionnaire-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'clark-coding-challenge/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list questionnaire');
+
+test('should list questionnaries.', function (assert) {
+	visit('/');
+
+	andThen(function() {
+		assert.equal(find('.questionnaires__item').length, 3, 'should see 3 listings');
+	});
+});
