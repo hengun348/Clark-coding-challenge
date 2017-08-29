5f9fd3026f19de4105e97dea6880fc3f82cb4495
Henrik Gunnarsson <hengun348@gmail.com>
2017-08-29 21:51:50 +0200

    feat(app): models for questionnaries and questions

---
+++ app/models/question.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	type: DS.attr('string'),
+	mandatory: DS.attr('boolean', { defaultValue: false }),
+	description: DS.attr('string'),
+	questionnaire: DS.belongsTo('questionnaire')
+});
