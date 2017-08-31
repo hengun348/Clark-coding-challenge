fdbb47a4d7b337e7ed9bd445e2bffe0d3f599077
Henrik Gunnarsson <hengun348@gmail.com>
2017-08-31 22:31:35 +0200

    feat(questionnaire model): add computed property

--- app/models/questionnaire.js
+++ app/models/questionnaire.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 	title: DS.attr('string'),
 	description: DS.attr('string'),
-	questions: DS.hasMany('question')
+	questions: DS.hasMany('question'),
+	numberOfQuestions: Ember.computed('questions', function() {
+		return this.get('questions.length');
+	})
 });
