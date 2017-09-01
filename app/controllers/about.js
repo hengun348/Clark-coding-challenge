20e626d9c1cb9b1a31df2f6d70523d159f3340f0
Henrik Gunnarsson <hengun348@gmail.com>
2017-09-01 15:34:37 +0200

    feat(about controller): use service to get first question

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	questionnaireHelper: Ember.inject.service('helper'),
+	questionnaireHelper: Ember.inject.service('questionnaire-helper'),
 	
 	actions: {
 		startQuestionnaire(questionnaire) {
+			const firstQuestion = this.get('questionnaireHelper').startAndReceiveFirstQuestion(questionnaire);
+			
+			if(firstQuestion) {
+				this.transitionToRoute('question', firstQuestion);
+			} else {
+				//Error
+			}
 		},
 		backToQuestionnaires() {
 			this.transitionToRoute('questionnaires');
