82b75e45429f6c1a8a5253e5103c424fd4418dd7
Henrik Gunnarsson <hengun348@gmail.com>
2017-09-01 15:33:01 +0200

    feat(app): add questionnaire-helper service

---
+++ app/services/questionnaire-helper.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+	currentQuestionnaire: null,
+	currentQuestionIndex: null,
+	questions: [],
+	numberOfQuestions: null,
+
+	startAndReceiveFirstQuestion(questionnaire) {
+		this.set('currentQuestion', questionnaire.id);
+		this.set('questions', questionnaire.get('questions'));
+		this.set('currentQuestionIndex', 0);
+		this.set('numberOfQuestions', questionnaire.get('questions.length'));
+
+		return questionnaire.get('questions')[0];
+	},
+	getCurrentQuestionIndex() {
+		return this.get('currentQuestionIndex');
+	},
+	nextQuestion() {
+		if(this.get('currentQuestionIndex') === this.get('numberOfQuestions') - 1) {
+			return;
+		}
+
+		this.incrementProperty('currentQuestionIndex');
+
+		return this.get('questions')[this.get('currentQuestionIndex')];
+	},
+	previousQuestion() {
+		if(this.get('currentQuestionIndex') === 0) {
+			return;
+		}
+
+		this.decrementProperty('currentQuestionIndex');
+		return this.get('questions')[this.get('currentQuestionIndex')];
+	}
+});
