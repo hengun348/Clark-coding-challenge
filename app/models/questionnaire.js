7c938ecdbdfab793cdee4fb0d03705ebf8c0890b
Henrik Gunnarsson <hengun348@gmail.com>
2017-09-01 15:43:43 +0200

    fix(questionnaire model): no type on questions

--- app/models/questionnaire.js
+++ app/models/questionnaire.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default DS.Model.extend({
 	title: DS.attr('string'),
 	description: DS.attr('string'),
-	questions: DS.hasMany('question'),
+	questions: DS.attr(),
 	numberOfQuestions: Ember.computed('questions', function() {
 		return this.get('questions.length');
 	})
