81a4901eca99a81a2649968140aa301ffcd69939
Henrik Gunnarsson <hengun348@gmail.com>
2017-09-02 19:36:58 +0200

    chore(question router) indentation

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,9 +4,9 @@ export default Ember.Route.extend({
 	model(params) {
 		const question = this.store.peekRecord('question', params.question_id);
 		
-			if(!question) {
-				this.transitionTo('questionnaires');
-			}
+		if(!question) {
+			this.transitionTo('questionnaires');
+		}
 		
 		return question;
 	}
