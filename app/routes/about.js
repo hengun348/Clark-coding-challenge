2f92907dc20399eb4872073d95e06e2fb3f8989e
Henrik Gunnarsson <hengun348@gmail.com>
2017-09-02 10:39:37 +0200

    fix(app): handle no route data
    - problem: user reloads page on a route that requires data to have been loaded in previous routes => empty template
    
    - solution: redirect to questionnaires page if this occurs

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(params) {
+		const questionnaire = this.store.peekRecord('questionnaire', params.questionnaire_id);
+
+		if(!questionnaire) {
+			this.transitionTo('questionnaires');
+		}
+
+		return questionnaire;
+	}
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(params) {
+		const question = this.store.peekRecord('question', params.question_id);
+		
+			if(!question) {
+				this.transitionTo('questionnaires');
+			}
+		
+		return question;
+	}
 });
